704e5d15eff123a64794473bf4a4946ee3ca39b6
Ben Kabongo <kabongo.ben025@gmail.com>
2023-12-20 13:17:56 +0100

    Gaussian Process fixed

--- src/gaussian_process.py
+++ src/gaussian_process.py
@@ -1,7 +1,11 @@
 import numpy as np
+import scipy.linalg
 import scipy.optimize
 from kernels import Kernel, ExponentiatedQuadraticKernel
-from utils import compute_inv_Psi_individual_i, concatenate_Theta_Sigma_i, retrieve_Theta_Sigma_i, log_likelihood_GP
+from utils import  (compute_inv_Psi_individual_i, 
+                    concatenate_Theta_Sigma_i, 
+                    retrieve_Theta_Sigma_i, 
+                    log_likelihood_learn_new_parameters)
 from typing import *
 
 
@@ -9,15 +13,15 @@ class GaussianProcess:
     """Gaussian Process"""
 
     def __init__(self, 
-                mean_function: Callable=lambda x: 0, 
-                kernel_k: Kernel=ExponentiatedQuadraticKernel,
+                mean_function: Callable=lambda x: np.zeros_like(x), 
+                kernel: Kernel=ExponentiatedQuadraticKernel,
                 theta: Union[int, float, list, np.ndarray]=np.array([1., 1.]),
                 sigma: Union[int, float]=1.,
                 T: Union[list, np.ndarray]=None,
                 Y: Union[list, np.ndarray]=None):
 
         self.mean_function = mean_function
-        self.kernel_k = kernel_k
+        self.kernel = kernel
         self.theta = theta
         assert sigma > 0
         self.sigma = sigma
@@ -29,38 +33,35 @@ class GaussianProcess:
         assert len(T) == len(Y)
         self.T = T if isinstance(T, np.ndarray) else np.array(T)
         self.Y = Y if isinstance(Y, np.ndarray) else np.array(Y)
-
+        self.mean = self.mean_function(self.T)
         theta_sigma0 = concatenate_Theta_Sigma_i(self.theta, self.sigma)
         theta_sigma = scipy.optimize.minimize(
-            fun=lambda x: log_likelihood_GP(x, self.kernel_k, self.T, self.Y, minimize=True, derivative=True),
+            fun=lambda x: log_likelihood_learn_new_parameters(x, self.kernel, self.T, self.Y, self.mean,
+                                                            minimize=True, derivative=True),
             jac=True,
             x0=theta_sigma0,
             method='L-BFGS-B',
-            options={'disp': True}
+            options={'disp': False}
         ).x
-        
         theta , sigma = retrieve_Theta_Sigma_i(theta_sigma)
         self.theta = theta
         self.sigma = sigma 
-        self.K = self.kernel_k.compute(self.theta, self.T, self.T)
-        self.C = self.K + (self.sigma) * np.eye(len(self.T))
-        self.inv_C = scipy.linalg.pinv(self.C) + 1e-6 * np.eye(len(self.T))
-        
-    def get_GP_params(self):
-        return np.zeros_like(self.T), self.C
 
 
     def predict(self, T_p):
         if self.T is None or self.Y is None:
             raise ValueError("The model must be trained before making predictions.")
-        print(self.theta, self.sigma)
-        predictions = []
-        for t_p in T_p:
-            c = self.kernel_k.compute(self.theta, t_p, t_p) + (self.sigma)
-            k = self.kernel_k.compute(self.theta, self.T, t_p)
-            mean = (k.T).dot(self.inv_C).dot(self.Y)
-            variance = c - (k.T).dot(self.inv_C).dot(k)
-            y = scipy.stats.norm(mean, np.sqrt(variance)).rvs()
-            predictions.append(y)
 
-        return predictions
+        n = len(self.T)
+        all_T = np.concatenate([self.T, T_p])
+        Sigma, _ = compute_inv_Psi_individual_i(self.kernel, self.theta, self.sigma, all_T, None)
+
+        K = Sigma[:n, :n] + 1e-6 * np.identity(n)
+        inv_K = scipy.linalg.pinv(K)
+        K_star = Sigma[:n, n:]
+        K_star_star = Sigma[n:, n:]
+
+        mean = self.mean_function(T_p) + K_star.T.dot(inv_K).dot(self.Y - self.mean_function(self.T))
+        covariance = K_star_star - K_star.T.dot(inv_K).dot(K_star)
+
+        return mean, covariance
